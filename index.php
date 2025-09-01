9e73042eb8d54089d58401c5fff685db4a75e6ef
Luiza Thier <lu.thier02@gmail.com>
2025-09-01 08:55:18 -0300

    index

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php 
-// include dos arquivox
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sistema Empresarial</title>
+  <link rel="stylesheet" href="./assets/style.css?v=<?php echo date("YmdHis").rand(0,9999999);?>">
+  <style>
+    
+  </style>
+</head>
+<body>
+  <header>
+    <h1>Sistema de Gestão de Empresa</h1>
+    <nav id="menu" >
+        <a href="./lista-cargos.php">Cargos</a>
+        <a href="./lista-setores.php">Setores</a>
+        <a href="./lista-funcionarios.php">Funcionários</a>
+        <a href="./lista-categorias.php">Categorias</a>
+        <a href="./lista-produtos.php">Produtos</a>
+        <a href="./lista-producao.php">Produção</a>
+        <a href="./lista-usuarios.php">Usuários</a>
+        <a href="./relatorios.php">Relatório</a>
+        <a href="./logoff.php">Sair</a>
+      </nav>
+  </header>
+
 include_once './include/logado.php';
 include_once './include/conexao.php';
 include_once './include/header.php';
@@ -12,6 +38,8 @@ include_once './include/header.php';
    
   </main>
   <?php 
-  // include dos arquivox
+<script src="./assets/script.js"></script>
+</body>
+</html>
   include_once './include/footer.php';
   ?>
