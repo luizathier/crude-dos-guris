3af3b7ba8194b7585ad25e5d3abd795c45114351
Vitoria Carlosso <84900172049@senacrs.edu.br>
2025-09-19 09:05:12 -0300

    select categorias

--- salvar-produtos.php
+++ salvar-produtos.php
@@ -16,6 +16,16 @@ include_once './include/header.php';
           <textarea placeholder="Descrição"></textarea>
           <select>
             <option value="">Categoria</option>
+          <?php
+          $sql_cat = "SELECT CategoriaID, Nome FROM categorias ORDER BY Nome";
+          $result_cat = mysqli_query($conexao, $sql_cat);
+
+          if(mysqli_num_rows($result_cat) > 0){
+              while($row = mysqli_fetch_assoc($result_cat)){
+                  echo "<option value='".$row['CategoriaID']."'>".$row['Nome']."</option>";
+              }
+          }
+          ?>
           </select>
           <button type="submit">Salvar</button>
         </form>
