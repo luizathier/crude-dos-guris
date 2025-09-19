6f628053078b508da337b88eb5612e313c0f95c7
Vitoria Carlosso <84900172049@senacrs.edu.br>
2025-09-19 09:02:02 -0300

    select do funcionarios

--- salvar-funcionarios.php
+++ salvar-funcionarios.php
@@ -23,10 +23,31 @@ include_once './include/header.php';
           <input type="text" placeholder="CPF">
           <input type="text" placeholder="RG">
           <select>
-            <option value="">Cargo</option>
+          <option value="">Cargo</option>
+          <?php
+          $sql_cargo = "SELECT CargoID, Nome FROM cargos ORDER BY Nome";
+          $result_cargo = mysqli_query($conexao, $sql_cargo);
+
+          if(mysqli_num_rows($result_cargo) > 0){
+              while($row = mysqli_fetch_assoc($result_cargo)){
+                  echo "<option value='".$row['CargoID']."'>".$row['Nome']."</option>";
+              }
+          }
+          ?>
           </select>
           <select>
             <option value="">Setor</option>
+            <option value="">Setor</option>
+          <?php
+          $sql_setor = "SELECT SetorID, Nome FROM setor ORDER BY Nome";
+          $result_setor = mysqli_query($conexao, $sql_setor);
+
+          if(mysqli_num_rows($result_setor) > 0){
+              while($row = mysqli_fetch_assoc($result_setor)){
+                  echo "<option value='".$row['SetorID']."'>".$row['Nome']."</option>";
+              }
+          }
+          ?>
           </select>
           <button type="submit">Salvar</button>
         </form>
